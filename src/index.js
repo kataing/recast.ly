1b5cf7a9ca9fd4b5638096d436f0157dcd5eb35b
kataing <kataing12@gmail.com>
2019-06-10 17:14:40 -0700

    First commit, able to render all components working on functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 // TODO: Render the `App` component to the DOM
 import App from './components/App.js';
+import Search from './components/Search.js';
 
+
+ReactDOM.render(<App />, document.getElementById('app'));
+// ReactDOM.render(<VideoList videos={exampleVideoData} />, document.getElementById('videoList'));
+// ReactDOM.render(<VideoPlayer video={exampleVideoData[0]}/> , document.getElementById('videoPlayer'));
